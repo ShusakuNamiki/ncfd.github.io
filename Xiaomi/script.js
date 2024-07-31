c7725653fff967f2b79a05e738491b07c2f30b19
ShusakuNamiki <uqmkh.pv@outlook.jp>
2024-07-31 23:43:09 +0900

    Create script.js

---
+++ Xiaomi/script.js
@@ -0,0 +1,6 @@
+const easterEgg = document.getElementById('easter-egg');
+const xiaomiRingtone = document.getElementById('xiaomi-ringtone');
+
+easterEgg.addEventListener('click', () => {
+  xiaomiRingtone.play();
+});
