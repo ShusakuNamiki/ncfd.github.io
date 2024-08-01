7dbd2364591918897668706223cccf83f2c3e486
ShusakuNamiki <uqmkh.pv@outlook.jp>
2024-08-01 13:37:01 +0900

    Update script.js

--- Hiyokomame0144/script.js
+++ Hiyokomame0144/script.js
@@ -1,13 +1,6 @@
-// ... (ゲームロジック) ...
-
-// スネークの描画
-function drawSnake() {
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i === 0) ? "red" : "green"; // 頭は赤、体は緑
-        ctx.fillRect(snake[i].x, snake[i].y, tileSize, tileSize);
-        ctx.strokeStyle = "white"; // 白枠
-        ctx.strokeRect(snake[i].x, snake[i].y, tileSize, tileSize);
-    }
-}
-
-// ... (その他のゲームロジック) ...
+gsap.to("#image-container img", {
+  duration: 1,  // アニメーション時間
+  opacity: 1,   // 表示
+  yPercent: 100, // 下に移動
+  ease: "power2.out" // イージング
+});
